20b936b52d2934c07127a47f43adadc054518cee
luaGeeko <hop2work@gmail.com>
2019-06-09 23:03:04 +0200

    experiments

--- poisson_spike_gen.py
+++ poisson_spike_gen.py
@@ -1,5 +1,11 @@
 import numpy as np
 import sklearn
+import matplotlib.pyplot as plt
+import coloredlogs, logging
+
+
+logger = logging.getLogger("poisson_Gen")
+coloredlogs.install(level='INFO')
 
 """ Tester Poisson spike generator """
 
@@ -10,7 +16,46 @@ firing_rate = 100      # Hz
 dt = 1 / 1000          # seconds
 total_interval = 10    # simulation for 10 ms
 
-# generate random numbers uniform distribution [0, 1]
-x = np.random.uniform(0, 1, total_interval)
-poisson_spike_train = np.where(x < firing_rate * dt, 1, 0)
-print (poisson_spike_train)
+
+def plotter(spikemat, time_dur):
+    for id in range(spikemat.shape[0]):
+        spikepos = np.take(time_dur, np.where(spikemat[id, :] == 1))
+        plt.plot(spikepos, id, '|', c='k')
+
+
+    plt.ylim(top=spikemat.shape[0]+1)
+    plt.plot([0]*20, [i for i in range(20)], c='blue')
+    plt.xlabel('Time(ms)')
+    plt.ylabel('Trial number')
+    plt.show()
+
+def poisson_spike_generator(fr, sim_len, n_trials):
+    dt = 1 / 1000    # we need to convert it to seconds dt is taken to be 1 ms
+    total_interval = int(np.floor(sim_len / dt))
+    raw_data = np.random.rand(n_trials, total_interval)
+    spike_mat = np.where(raw_data < fr * dt, 1, 0)
+    end_val = sim_len - dt
+    time_vec = np.linspace(0, end_val, num=total_interval)
+    return spike_mat, time_vec
+
+
+def homogenous_poisson_gen():
+    """ firing rate constant over time """
+    pass
+
+def inhomogenous_poisson_gen():
+    """ time dependent firing rate """
+    pass
+
+################# Baseline of 6 hz and after presentation of moving stimulus, firing rate of this neuron increases to 30 Hz ############
+matdata, timedure = poisson_spike_generator(30, 1, 20)
+basmat, bastime = poisson_spike_generator(6, 0.5, 20)
+bastime = (bastime - bastime[-1]) * 1000 - 1
+
+# concatenate columns wise
+result_mat = np.concatenate((basmat, matdata), axis=1)
+result_time = np.concatenate((bastime, timedure*1000))
+
+plotter(result_mat, result_time)
+
+##################
